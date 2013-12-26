447267158d23463ce4c953468698b6b5e207c29a
Philip Szalwinski <pszalwinski@gmail.com>
2013-12-26 10:19:08 -0600

    Add status to client model

--- lib/client_model.rb
+++ lib/client_model.rb
@@ -2,6 +2,7 @@ class ClientModel
   include DataMapper::Resource
   property :id, Serial
   property :name, String
+  property :status, String
 
   property :created_at, DateTime
   property :updated_at, DateTime
