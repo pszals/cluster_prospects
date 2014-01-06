363ad78e92fa8f0246b8cbd679acd3809eff9f71
Philip Szalwinski <pszalwinski@gmail.com>
2014-01-05 19:35:01 -0600

    Renames app

---
+++ lib/cluster_app.rb
@@ -0,0 +1,110 @@
+$LOAD_PATH.unshift(File.dirname(__FILE__))
+
+require 'sinatra'
+require 'bundler'
+require 'data_mapper'
+require 'client_service'
+require 'client_model'
+require 'task_model'
+
+DataMapper.setup(:default, ENV["HEROKU_POSTGRESQL_ROSE_URL"] || "postgres://pszalwinski: @localhost/cluster")
+DataMapper.auto_upgrade!
+DataMapper.finalize
+
+
+class Sinatra_Cluster < Sinatra::Base
+  attr_reader :params, :client_manager
+
+  use Rack::Auth::Basic do |username, password|
+    username == 'margaret' && password == 't4sktr4ck3r'
+  end
+
+  def client_service
+    @client_service ||= ClientService.new(ClientModel, TaskModel)
+  end
+
+  get '/' do
+    @clients = client_service.ascending_name
+    client_service.tasks_by_descending_priority
+    erb :home
+  end
+
+  get '/all_tasks' do
+    @clients = client_service.ascending_name
+    client_service.tasks_by_descending_priority
+    erb :all_tasks
+  end
+
+  post '/complete_task' do
+    @clients = client_service.all
+    client_service.complete_task(params["client_id"], params["task_id"])
+    redirect '/all_tasks'
+  end
+  
+  post '/' do
+    @clients = client_service.all
+    client_service.complete_task(params["client_id"], params["task_id"])
+    redirect '/'
+  end
+
+  get '/all_clients' do
+    @clients = client_service.get_active_clients
+    erb :all_clients
+  end
+
+  post '/all_clients' do
+    client_service.update_client_status(params["client_id"].to_i, params[:status])
+    @clients = client_service.get_active_clients
+    erb :all_clients
+  end
+
+  get '/prospects' do
+    @clients = client_service.get_prospects
+    erb :prospects
+  end
+
+  post '/prospects' do
+    client_service.update_client_status(params["client_id"].to_i, params[:status])
+    @clients = client_service.get_prospects
+    erb :prospects
+  end
+
+  get '/dormant' do
+    @clients = client_service.get_dormant_clients
+    erb :dormant
+  end
+
+  post '/dormant' do
+    client_service.update_client_status(params["client_id"].to_i, params[:status])
+    @clients = client_service.get_dormant_clients
+    erb :dormant
+  end
+
+  get '/add_client' do
+    @clients = client_service.ascending_name
+    erb :add_client
+  end
+
+  post '/add_client' do
+    client_service.make_new_client(params[:new_client], params[:status])
+    @clients = client_service.all
+    redirect '/'
+  end
+
+  get '/add_task' do
+    @clients = client_service.ascending_name
+    erb :add_task
+  end
+  
+  post '/add_task' do
+    @clients = client_service.ascending_name
+    client_service.add_task(params[:client_id], params[:task], params[:status])
+    erb :home
+  end
+
+  get '/:id' do 
+    @clients = [client_service.get_by_id(params[:id])]
+    client_service.tasks_by_descending_priority
+    erb :all_tasks
+  end
+end
---
+++ spec/cluster_app_spec.rb
@@ -0,0 +1,107 @@
+require 'cluster_app'
+require 'spec_helper'
+require 'data_mapper'
+require 'client_model'
+require 'task_model'
+
+DataMapper.setup(:default, "postgres://pszalwinski: @localhost/cluster")
+DataMapper.finalize
+DataMapper.auto_upgrade!
+
+def app
+  Sinatra_Cluster
+end
+
+describe Sinatra_Cluster do
+  let(:cluster) { Sinatra_Cluster.new }
+  before(:each) do
+    authorize 'margaret', 't4sktr4ck3r'
+  end
+
+  describe 'cluster home page' do
+    it 'retrieves a 200 response' do
+      get '/'
+      last_response.status.should == 200
+    end
+  end
+
+  describe 'viewing all tasks' do
+    it 'retrieves a 200 response' do
+      get '/all_tasks'
+      last_response.status.should == 200
+    end
+  end
+
+  describe 'completing a task' do
+    it 'completes a task' do
+      test_client = ClientModel.create(:name => "test task completion")
+      TaskModel.create(:description => "test task completion", :client_model => test_client, :priority => 3)
+      client_id = ClientModel.first(:name => "test task completion").id
+      task_id = TaskModel.first(:description => "test task completion").id
+      post '/complete_task', params={:client_id => client_id, :task_id => task_id}
+
+      TaskModel.get(task_id).completed.should be_true
+      last_response.status.should == 302
+
+      TaskModel.all(:description => "test task completion").destroy
+      ClientModel.all(:name => "test task completion").destroy
+    end
+  end
+
+  describe 'viewing all clients' do
+    it 'retrieves a 200 response' do
+      get '/add_client'
+      last_response.status.should == 200
+    end
+  end
+
+  describe 'viewing all prospects' do
+    it 'retrieves a 200 response' do
+      get '/prospects'
+      last_response.status.should == 200
+    end
+  end
+
+  describe 'adding a client' do
+    it 'creates a new client' do
+      post '/add_client', params={:new_client => "Test Client from sinatra_cluster_spec.rb"}
+
+      ClientModel.first(:name => "Test Client from sinatra_cluster_spec.rb").should_not be_nil
+      last_response.status.should == 302
+
+      ClientModel.all(:name => "Test Client from sinatra_cluster_spec.rb").destroy
+    end
+
+    it 'adds client status' do
+      post '/add_client', params={:new_client => "Test Client", :status => "prospect"}
+
+      ClientModel.first(:status => "prospect").should_not be nil
+      ClientModel.all(:name => "Test Client").destroy
+    end
+  end
+
+  describe 'all_clients' do
+    it 'modifies the status of a client' do
+      ClientModel.create(:name => "Test Client")
+      fake_id = ClientModel.first(:name => "Test Client").id
+      post '/all_clients', params={:status => "prospect", :client_id => fake_id} 
+
+      ClientModel.first(:id => fake_id).status.should == "prospect"
+      ClientModel.all(:name => "Test Client").destroy
+    end
+  end
+
+  describe 'adding a task' do
+    it 'retrieves a 200 response' do
+      ClientModel.create(:name => "test adding a task")
+      client_id = ClientModel.first(:name => "test adding a task").id
+      post '/add_task', params={:client_id => client_id, :task => "test adding a task", :priority => 3}
+
+      last_response.status.should == 200
+      TaskModel.first(:description => "test adding a task").should_not be_nil
+
+      TaskModel.all(:description => "test adding a task").destroy
+      ClientModel.all(:name => "test adding a task").destroy
+    end
+  end
+end
