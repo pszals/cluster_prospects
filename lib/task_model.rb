d8f1179faa09c46477902a9c413f3f2545451ce8
Philip Szalwinski <pszalwinski@gmail.com>
2014-01-19 13:33:02 -0600

    Tasks can store created_by and completed_by attribute

--- lib/task_model.rb
+++ lib/task_model.rb
@@ -4,6 +4,8 @@ class TaskModel
   property :priority, Integer
   property :description, Text
   property :completed, Boolean, :default => false
+  property :created_by, Integer, required: false
+  property :completed_by, Integer, required: false
 
   property :created_at, DateTime
   property :updated_at, DateTime
